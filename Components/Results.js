edbdd91cc91f36160a963bf85eef2ba9d956998e
Avimistry4800 <avimistry4800@gmail.com>
2021-06-09 20:37:46 +0600

    :( was forget to create git repo

---
+++ Components/Results.js
@@ -0,0 +1,14 @@
+import requests from "../utls/requests"
+import Thumbnail from "./Thumbnail"
+
+function Results({results}) {
+    return (
+        <div className="px-5 my-10 sm:grid md-grid-cols-2">
+            {results.map(result => (
+                <Thumbnail key={result.id} result={result}/>
+            ))}
+        </div>
+    )
+}
+
+export default Results
